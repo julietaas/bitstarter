e38e850422f9b35f6f82ee4f495e115fcb0b6c04
Julia Agudo <julietaas@gmail.com>
2013-07-09 21:59:35 +0000

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf;
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // Response.send('Hello World 2!');
+  buf = fs.readFileSync('./index.html');
+  Response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
